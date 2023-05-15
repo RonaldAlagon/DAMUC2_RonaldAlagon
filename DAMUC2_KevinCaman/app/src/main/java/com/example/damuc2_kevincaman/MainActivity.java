8069153c4a050a4519e213ee5fa0a7a1aeae253d
Diego Quispe <20a3010378@untels.edu.pe>
2023-05-15 16:28:54 -0500

    segunda parte quispe cancho

--- DAMUC2_KevinCaman/app/src/main/java/com/example/damuc2_kevincaman/MainActivity.java
+++ DAMUC2_KevinCaman/app/src/main/java/com/example/damuc2_kevincaman/MainActivity.java
@@ -3,12 +3,17 @@ package com.example.damuc2_kevincaman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.BtnEnviar);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Datos Enviados", Toast.LENGTH_SHORT).show());
+
     }
 }
